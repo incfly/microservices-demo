5aac70ed4dab47f1239f37998d95b684f7aba98a
Jianfei Hu <jianfeih@google.com>
2019-03-26 17:21:28 -0700

    traffic splitting demo and python script count.py

---
+++ istio-gce/counting.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+from lxml import html
+import requests
+
+def send_request():
+  page = requests.get('http://35.226.99.75/')
+  tree = html.fromstring(page.content)
+  elements = tree.xpath('/html/body/main/div/div/div/div/div/div/h5')
+  return [e.text.strip() for e in elements]
+
+vm = 0
+kubernetes = 0
+for i in xrange(3000):
+  products = send_request()
+  if len(products) == 1:
+    vm += 1
+  else:
+    kubernetes += 1
+  print "Kubernetes vs VM requests handled %d / %d, products: %s" % (kubernetes, vm, products)
